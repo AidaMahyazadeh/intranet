4d96d7fa6a741995653d4ac0e1c14a6ed3631158
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-16 11:27:02 +0200

    added some routes.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { adminAuthGuard } from './core/services/guards/admin-auth.guard';
+import { LoginComponent } from './shared/login/login.component';
+import { SignupComponent } from './shared/signup/signup.component';
+import { HomeComponent } from './shared/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', redirectTo :'login',pathMatch:'full'}, 
+  {path: 'login',component:LoginComponent},
+  {path :'signup',component :SignupComponent},
+  {path : 'home' ,component :HomeComponent},
+  {path :'admin',canActivate :[adminAuthGuard] ,loadChildren:()=>import('./admin/admin.module').then(module=>module.AdminModule)},
+  {
+    path :'courses',loadChildren :()=>import('./courses/courses.module').then(module=>module.CoursesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
