7e2f18229394e294a118e9c90d997d0f1f8c8c8f
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-17 20:12:45 +0200

    used angular material.

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -1,12 +1,53 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AdminRoutingModule } from './admin-routing-module';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
+
+import { AdminComponent } from './admin.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { CourselistComponent } from './courselist/courselist.component';
+import { ProfessorsComponent } from './professors/professors.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MatCardModule } from '@angular/material/card';
+import { NewProfessorComponent } from './new-professor/new-professor.component';
+import { ModifyDetailProfessorComponent } from './modify-detail-professor/modify-detail-professor.component';
+
+
+
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminComponent,
+    UserlistComponent,
+    CourselistComponent,
+    ProfessorsComponent,
+    SidebarComponent,
+    NewProfessorComponent,
+    ModifyDetailProfessorComponent
+   
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule
   ]
 })
 export class AdminModule { }
---
+++ frontend/src/app/admin/sidebar/sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthStorageService } from 'src/app/core/services/auth/auth-storage.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  adminName:string ='';
+  constructor(private authStorageService : AuthStorageService){}
+
+  ngOnInit(): void {
+    this.adminName= this.authStorageService.getUserName()
+  }
+}
