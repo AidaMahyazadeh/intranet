125ee242785cde88b749fdea69b2b31903ad87a1
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-24 12:46:39 +0200

    opened modal for modifying courses.

--- frontend/src/app/admin/courselist/courselist.component.ts
+++ frontend/src/app/admin/courselist/courselist.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import ICourse from 'src/app/core/models/course.model';
 import { CoursesListService } from 'src/app/core/services/admin/courses-list.service';
+import { ModifyCoursesComponent } from '../modify-courses/modify-courses.component';
+
 
 @Component({
   selector: 'app-courselist',
@@ -8,18 +11,38 @@ import { CoursesListService } from 'src/app/core/services/admin/courses-list.ser
   styleUrls: ['./courselist.component.css']
 })
 export class CourselistComponent implements OnInit{
-  displayedColumns=['id','name', 'author','image','description','duration','delete']
+  // displayedColumns=['id','name', 'author','image','description','duration','delete']
   courses !:ICourse[];
+  selectedCourse !:ICourse;
 
-  constructor(private coursesListService:CoursesListService){}
+  constructor(
+    private coursesListService:CoursesListService,
+    private dialog :MatDialog
+    ){}
   
   ngOnInit() {
     this.coursesListService.getAllCourses().subscribe(
       res=>this.courses=res
     )
   }
+
+  getCourseById(courseId:number){
+   return this.selectedCourse=this.courses.find(course=>course.id==courseId)!
+  }
   
   onDelete(id:number){
     this.courses= this.courses.filter(course=>course.id!=id)
+  }
+
+  openDialog(){
+  //  const dialogRef= this.dialog.open()
+  }
+
+  openModify(id:number){
+    this.getCourseById(id)
+    const dialogRef= this.dialog.open(ModifyCoursesComponent,{
+      height:'100%'
+    })
+    dialogRef.componentInstance.selectedCourse=this.selectedCourse
   }
 }
