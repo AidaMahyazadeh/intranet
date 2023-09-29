f4292264030d494cd05d35fb09481d5170b722cb
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-29 09:38:10 +0200

    imported pagesModule.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -21,7 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     CoreModule,
-    SharedModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
