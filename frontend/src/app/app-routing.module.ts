c0478b963523f73f4c6ff9add2a89aaae2692a3c
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-29 09:37:53 +0200

    removed some routes&moved into pagesRouting.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { adminAuthGuard } from './core/services/guards/admin-auth.guard';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { HomeComponent } from './pages/home/home.component';
+
 
 const routes: Routes = [
   {path : '', redirectTo :'login',pathMatch:'full'}, 
-  // {path: 'login',component:LoginComponent},
-  // {path :'signup',component :SignupComponent},
-  // {path : 'home' ,component :HomeComponent},
   {path :'admin',canActivate :[adminAuthGuard] ,loadChildren:()=>import('./admin/admin.module').then(module=>module.AdminModule)},
   {
     path :'courses',loadChildren :()=>import('./courses/courses.module').then(module=>module.CoursesModule)
